8d4c79df257a5a1a2973d047236f2910542a4aa1
Aman Anand <amannand.work@gmail.com>
2024-07-24 14:52:19 +0000

    task1-5

---
+++ Helpers/GlobalFunctions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sage50Connector.Helpers
+{
+    static class GlobalFunctions
+    {
+        public static object loadValue(object currentObject, string[] pathElements)
+        {
+
+            foreach (var element in pathElements)
+            {
+                if (currentObject == null)
+                {
+                    return null;
+                }
+                var tempMethod = currentObject.GetType().GetMethod(element);
+                if (tempMethod != null)
+                {
+                    currentObject = tempMethod.Invoke(currentObject, null);
+                    continue;
+                }
+
+                var property = currentObject.GetType().GetProperty(element);
+                if (property == null)
+                {
+                    return null;
+                }
+
+                currentObject = property.GetValue(currentObject);
+                if (currentObject == null)
+                {
+                    return null;
+                }
+            }
+
+            return currentObject;
+        }
+    }
+}
