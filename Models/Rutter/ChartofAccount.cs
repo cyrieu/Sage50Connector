fc682f14c312090ffeede654d50e08e67cc8dc70
Aman Anand <amannand.work@gmail.com>
2024-07-24 14:52:19 +0000

    task1-5

--- Models/Rutter/ChartofAccount.cs
+++ Models/Rutter/ChartofAccount.cs
@@ -1,17 +1,22 @@
 ﻿using Sage.Peachtree.API;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace Sage50Connector.Models.Rutter
 {
     public class ChartofAccount
     {
-        public string ID { get; set; }
-        public string Description { get; set; }
         public string Classification { get; set; }
+        public string Description { get; set; }
+        public string ID { get; set; }
+        public bool IsAdded { get; set; }
+        public bool IsDeleteAllowed { get; set; }
+        public bool IsDeleted { get; set; }
         public bool IsInactive { get; set; }
+        public bool IsModified { get; set; }
+        public bool IsSaveAllowed { get; set; }
+        public bool IsUnchanged { get; set; }
+        public EntityReference<Account> Key { get; set; }
+        public int Revision { get; set; }
     }
 }
---
+++ Models/Rutter/ChartofCustomer.cs
@@ -0,0 +1,45 @@
+﻿using Sage.Peachtree.API;
+using System;
+using System.Collections.Generic;
+
+namespace Sage50Connector.Models.Rutter
+{
+    public class ChartofCustomer
+    {
+        public string AccountNumber { get; set; }
+        public int AverageDaysToPayInvoices { get; set; }
+        public decimal Balance { get; set; }
+        public Contact BillToContact { get; set; }
+        public EntityReference<Account> CashAccountReference { get; set; }
+        public string Category { get; set; }
+        public List<Contact> Contacts { get; set; }
+        public string CreditStatus { get; set; }
+        public DateTime CustomerSince { get; set; }
+        public Dictionary<string, string> CustomFieldValues { get; set; }
+        public string Email { get; set; }
+        public string ID { get; set; }
+        public bool IsInactive { get; set; }
+        public bool IsProspect { get; set; }
+        public decimal LastInvoiceAmount { get; set; }
+        public DateTime? LastInvoiceDate { get; set; }
+        public decimal LastPaymentAmount { get; set; }
+        public DateTime? LastPaymentDate { get; set; }
+        public DateTime? LastSavedAt { get; set; }
+        public DateTime? LastStatementDate { get; set; }
+        public string Name { get; set; }
+        public string OpenPurchaseOrderNumber { get; set; }
+        public string PaymentMethod { get; set; }
+        public List<PhoneNumber> PhoneNumbers { get; set; }
+        public string PriceLevel { get; set; }
+        public bool ReplaceInventoryItemIDWithPartNumber { get; set; }
+        public bool ReplaceInventoryItemIDWithUPC { get; set; }
+        public string ResaleNumber { get; set; }
+     //   public EmployeeReference SalesRepresentativeReference { get; set; }
+        public Contact ShipToContact { get; set; }
+        public string ShipVia { get; set; }
+        public string Terms { get; set; }
+        public bool UseEmailToDeliverForms { get; set; }
+        public EntityReference<Account> UsualSalesAccountReference { get; set; }
+        public string WebSiteURL { get; set; }
+    }
+}
